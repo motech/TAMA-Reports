19d01c8315274ad0376bf4ab76435b29f28bc965
Balaji Narain <balajin@thoughtworks.com>
2013-01-24 11:37:16 +0530

    BalajiN | #5 | Increasing width of the gender column

--- tama-reports-migration/src/main/resources/db/migration/V1_1.sql
+++ tama-reports-migration/src/main/resources/db/migration/V1_1.sql
@@ -3,7 +3,7 @@ create table tama_reports.patient (
     patient_doc_id varchar(100),
     patient_id varchar(100),
     date_of_birth date,
-    gender varchar(1),
+    gender varchar(10),
     clinic_id varchar(100),
     travel_time_to_clinic varchar(100),
     ivr_pass_code varchar(10),
