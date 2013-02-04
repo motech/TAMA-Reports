c737d06b5923a83b98f73e471e10021151e1f1f7
Balaji Narain <balajin@thoughtworks.com>
2013-02-04 18:51:27 +0530

    BalajiN | #0 | Using text instead of varchar

--- tama-reports-migration/src/main/resources/db/migration/V1_1.sql
+++ tama-reports-migration/src/main/resources/db/migration/V1_1.sql
@@ -28,15 +28,15 @@ create table tama_reports.medical_history (
 
     patient_id varchar(100),
 
-    test_reason varchar(100),
-    modes_of_transmission varchar(100),
+    test_reason text,
+    modes_of_transmission text,
 
     history_of_drug_allergy varchar(20),
     history_of_rash varchar(20),
     sulfonamide_allergy varchar(20),
     arv_allergy varchar(20),
     other varchar(3),
-    other_details varchar(100),
+    other_details text,
     rash_to_delaviridine_or_rescriptor_or_dlv varchar(20),
     rash_to_efavirenz_or_sustiva_or_stocrin_or_efv varchar(20),
     pregnant varchar(20),
@@ -50,50 +50,50 @@ create table tama_reports.medical_history (
 
     allergic_dermititis_eczema varchar(20),
     inherited_childhood_eczema_dermititus varchar(20),
-    allergic_others varchar(100),
-    allergic_remarks varchar(100),
+    allergic_others text,
+    allergic_remarks text,
 
     dermatological varchar(20),
-    dermatological_remarks varchar(100),
+    dermatological_remarks text,
 
     respiratory varchar(20),
-    respiratory_remarks varchar(100),
+    respiratory_remarks text,
 
     coronary_disease varchar(20),
-    coronary_other varchar(100),
-    coronary_remarks varchar(100),
+    coronary_other text,
+    coronary_remarks text,
 
     endocrine varchar(20),
-    endocrine_remarks varchar(100),
+    endocrine_remarks text,
 
     gastrointestinal varchar(20),
-    gastrointestinal_remarks varchar(100),
+    gastrointestinal_remarks text,
 
     genito_urinary varchar(20),
-    genito_urinary_remarks varchar(100),
+    genito_urinary_remarks text,
 
     hematological varchar(20),
-    hematological_remarks varchar(100),
+    hematological_remarks text,
 
     musculo_skeletal varchar(20),
-    musculo_skeletal_remarks varchar(100),
+    musculo_skeletal_remarks text,
 
     dizziness varchar(20),
     insomnia varchar(20),
     impaired_concentration varchar(20),
     somnolence varchar(20),
     abnormal_dreams_nightmare varchar(20),
-    nuerological_others varchar(100),
-    nuerological_remarks varchar(100),
+    nuerological_others text,
+    nuerological_remarks text,
 
     psychiatric varchar(20),
-    psychiatric_remarks varchar(100),
+    psychiatric_remarks text,
 
     eyes varchar(20),
-    eyes_remarks varchar(100),
+    eyes_remarks text,
 
     ent varchar(20),
-    ent_remarks varchar(100),
+    ent_remarks text,
 
     hypertension varchar(20),
     nephrotoxicity varchar(20),
@@ -101,8 +101,8 @@ create table tama_reports.medical_history (
     tb varchar(20),
     alcoholism varchar(20),
 
-    non_hiv_other varchar(100),
-    non_hiv_other_remarks varchar(100)
+    non_hiv_other text,
+    non_hiv_other_remarks text
 );
 
 create table tama_reports.patient_event (
