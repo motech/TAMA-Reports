f6c81278a96832597629a97098029d55d6ea65f0
Balaji Narain <balajin@thoughtworks.com>
2013-02-08 15:20:47 +0530

    Shashank,BalajiN | #0 | Ensuring that patient registration details report does not fail under load

--- tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/export/ReportingService.java
+++ tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/export/ReportingService.java
@@ -1,8 +1,12 @@
 package org.motechproject.tama.reports.domain.export;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.fill.JRSwapFileVirtualizer;
 import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.engine.util.JRSwapFile;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,6 +14,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.Connection;
 
@@ -28,9 +33,30 @@ public class ReportingService {
     }
 
     public void export(ReportParameters parameters, OutputStream outputStream, String path) throws Exception {
+        JRSwapFileVirtualizer virtualizer = null;
         try (Connection connection = dataSource.getConnection()) {
-            JasperReport report = (JasperReport) JRLoader.loadObject(new ClassPathResource(path).getInputStream());
-            new TAMAExcelExporter().export(JasperFillManager.fillReport(report, parameters.parameters(), connection), outputStream);
+            virtualizer = createVirtualizer(virtualizer);
+            setUpParams(parameters, virtualizer);
+            export(parameters, outputStream, path, connection);
+        } finally {
+            if (null != virtualizer) {
+                virtualizer.cleanup();
+            }
         }
+    }
+
+    private void export(ReportParameters parameters, OutputStream outputStream, String path, Connection connection) throws JRException, IOException {
+        JasperReport report = (JasperReport) JRLoader.loadObject(new ClassPathResource(path).getInputStream());
+        new TAMAExcelExporter().export(JasperFillManager.fillReport(report, parameters.parameters(), connection), outputStream);
+    }
+
+    private void setUpParams(ReportParameters parameters, JRSwapFileVirtualizer virtualizer) {
+        parameters.parameters().put(JRParameter.REPORT_VIRTUALIZER, virtualizer);
+    }
+
+    private JRSwapFileVirtualizer createVirtualizer(JRSwapFileVirtualizer virtualizer) {
+        JRSwapFile swapFile = new JRSwapFile("/tmp", 1024, 100);
+        virtualizer = new JRSwapFileVirtualizer(50, swapFile, true);
+        return virtualizer;
     }
 }
--- tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/export/TAMAExcelExporter.java
+++ tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/export/TAMAExcelExporter.java
@@ -3,19 +3,21 @@ package org.motechproject.tama.reports.domain.export;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.export.JExcelApiExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
 import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
 
 import java.io.OutputStream;
 
 public class TAMAExcelExporter {
 
-    JExcelApiExporter exporterXLS;
+    JRXlsExporter exporterXLS;
 
     public TAMAExcelExporter() {
-        exporterXLS = new JExcelApiExporter();
+        exporterXLS = new JRXlsExporter();
         exporterXLS.setParameter(JRXlsExporterParameter.IS_ONE_PAGE_PER_SHEET, Boolean.TRUE);
+        exporterXLS.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
         exporterXLS.setParameter(JRXlsExporterParameter.IS_COLLAPSE_ROW_SPAN, Boolean.TRUE);
+        exporterXLS.setParameter(JRXlsExporterParameter.START_PAGE_INDEX, 0);
         exporterXLS.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
     }
 
