b5ea42f228ea8fd3ae2f20d3a4198b91e79fdc86
Balaji Narain <balajin@thoughtworks.com>
2013-02-11 19:19:52 +0530

    BalajiN | #16 | Handling null values correctly in PatientEventReport

--- tama-reports-mapping/src/main/java/org/motechproject/tama/reports/mapping/PatientEventRequestMapper.java
+++ tama-reports-mapping/src/main/java/org/motechproject/tama/reports/mapping/PatientEventRequestMapper.java
@@ -4,6 +4,8 @@ package org.motechproject.tama.reports.mapping;
 import org.motechproject.tama.reports.contract.PatientEventRequest;
 import org.motechproject.tama.reports.domain.PatientEvent;
 
+import static org.apache.commons.lang.StringUtils.isNotBlank;
+
 public class PatientEventRequestMapper {
 
     private PatientEventRequest request;
@@ -15,10 +17,14 @@ public class PatientEventRequestMapper {
     public PatientEvent map() {
         PatientEvent patientEvent = new PatientEvent();
         patientEvent.setPatientDocumentId(request.getPatientDocumentId());
-        patientEvent.setEventName(request.getEventName());
+        patientEvent.setEventName(nonEmptyStringValue(request.getEventName()));
         patientEvent.setDateTime(request.getDateTime());
-        patientEvent.setNewValue(request.getNewValue());
-        patientEvent.setPerformedBy(request.getPerformedBy());
+        patientEvent.setNewValue(nonEmptyStringValue(request.getNewValue()));
+        patientEvent.setPerformedBy(nonEmptyStringValue(request.getPerformedBy()));
         return patientEvent;
+    }
+
+    private String nonEmptyStringValue(String value) {
+        return isNotBlank(value) ? value : "";
     }
 }
---
+++ tama-reports-mapping/src/test/java/org/motechproject/tama/reports/mapping/PatientEventRequestMapperTest.java
@@ -0,0 +1,72 @@
+package org.motechproject.tama.reports.mapping;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.motechproject.tama.reports.contract.PatientEventRequest;
+import org.motechproject.tama.reports.domain.PatientEvent;
+
+import static org.junit.Assert.assertEquals;
+import static org.motechproject.util.DateUtil.now;
+
+public class PatientEventRequestMapperTest {
+
+    private PatientEventRequest request;
+    private PatientEventRequestMapper patientEventRequestMapper;
+
+    @Before
+    public void setup() {
+        initializeRequest();
+        patientEventRequestMapper = new PatientEventRequestMapper(request);
+    }
+
+    private void initializeRequest() {
+        request = new PatientEventRequest();
+        request.setEventName("eventName");
+        request.setNewValue("value");
+        request.setPerformedBy("user");
+        request.setPatientDocumentId("patientDocId");
+        request.setDateTime(now().toDate());
+    }
+
+    @Test
+    public void shouldMapPatientDocumentId() {
+        PatientEvent event = patientEventRequestMapper.map();
+        assertEquals(request.getPatientDocumentId(), event.getPatientDocumentId());
+    }
+
+    @Test
+    public void shouldMapPerformedBy() {
+        PatientEvent event = patientEventRequestMapper.map();
+        assertEquals(request.getPerformedBy(), event.getPerformedBy());
+
+        request.setPerformedBy("");
+        event = patientEventRequestMapper.map();
+        assertEquals("", event.getPerformedBy());
+    }
+
+    @Test
+    public void shouldMapDateTime() {
+        PatientEvent event = patientEventRequestMapper.map();
+        assertEquals(request.getDateTime(), event.getDateTime());
+    }
+
+    @Test
+    public void shouldMapPatientEventName() {
+        PatientEvent event = patientEventRequestMapper.map();
+        assertEquals(request.getEventName(), event.getEventName());
+
+        request.setEventName(null);
+        event = patientEventRequestMapper.map();
+        assertEquals("", event.getEventName());
+    }
+
+    @Test
+    public void shouldMapNewValue() {
+        PatientEvent event = patientEventRequestMapper.map();
+        assertEquals(request.getNewValue(), event.getNewValue());
+
+        request.setNewValue(null);
+        event = patientEventRequestMapper.map();
+        assertEquals("", event.getNewValue());
+    }
+}
