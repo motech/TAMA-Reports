28d8631825f20a594486fbb12175060c804b2640
Balaji Narain <balajin@thoughtworks.com>
2013-04-03 18:39:13 +0530

    BalajiN | #0 | Logging uncaught exceptions

---
+++ tama-reports-web/src/main/java/org/motechproject/tama/reports/web/UncaughtExceptionHandler.java
@@ -0,0 +1,23 @@
+package org.motechproject.tama.reports.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("/uncaughtException")
+@Controller
+public class UncaughtExceptionHandler {
+
+    private Logger logger = Logger.getLogger(UncaughtExceptionHandler.class);
+
+    @RequestMapping("handle")
+    public String handle(HttpServletRequest request, Model model) {
+        Throwable exception = (Throwable) request.getAttribute("javax.servlet.error.exception");
+        logger.error(exception);
+        model.addAttribute("exception", exception);
+        return "accessDenied";
+    }
+}
