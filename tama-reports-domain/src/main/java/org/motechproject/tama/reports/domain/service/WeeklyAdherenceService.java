487f2c4340321cffc933a37e60853f5c415d88a7
Vikram Rao <vikramra@thoughtworks.com>
2013-06-14 10:47:23 +0530

    Vikram | weekly missed dosage log

--- tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/service/WeeklyAdherenceService.java
+++ tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/service/WeeklyAdherenceService.java
@@ -1,9 +1,15 @@
 package org.motechproject.tama.reports.domain.service;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
 import org.motechproject.tama.reports.domain.WeeklyAdherence;
 import org.motechproject.tama.reports.domain.repository.AllWeeklyAdherence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 @Component
@@ -23,15 +29,57 @@ public class WeeklyAdherenceService {
 
     public void update(WeeklyAdherence weeklyAdherence) {
         WeeklyAdherence persistedWeeklyAdherence = new WeeklyAdherence();
-        //TODO REMOVE commented code
-        persistedWeeklyAdherence =allWeeklyAdherence.findWeeklyAdherenceById(weeklyAdherence.getPatientDocumentId(),weeklyAdherence.getClinicName(),
-                weeklyAdherence.getWeekStartDate().getTime());
+        List<WeeklyAdherence> weeklyAdherences = null;
+        weeklyAdherences = allWeeklyAdherence.findWeeklyAdherenceById(weeklyAdherence.getPatientDocumentId());
+        if (weeklyAdherences != null) {
+            if (weeklyAdherences.size() != 0) {
+                persistedWeeklyAdherence = weeklyAdherences.get(0);
+            }
+        }
+
+        //allWeeklyAdherence.findWeeklyAdherenceById(weeklyAdherence.getPatientDocumentId(),weeklyAdherence.getClinicName(),
+        // weeklyAdherence.getWeekStartDate().getTime());
         //new WeeklyAdherence();
 
         //allWeeklyAdherence.findSimilarWeeklyLog(weeklyAdherence.getPatientDocumentId());
         //, weeklyAdherence.getClinicName(), weeklyAdherence.getTreatmentAdviceId(), weeklyAdherence.getWeekStartDate().toString());
         persistedWeeklyAdherence.merge(weeklyAdherence);
         allWeeklyAdherence.save(persistedWeeklyAdherence);
+    }
+
+    private List<WeeklyAdherence> filterAlertsByClinicName(List<WeeklyAdherence> weeklyAdherences, String clinicName) {
+        List<WeeklyAdherence> filteredAlerts = new ArrayList();
+        CollectionUtils.select(weeklyAdherences, getSelectorForClinic(clinicName), filteredAlerts);
+        return filteredAlerts;
+
+    }
+
+    private List<WeeklyAdherence> filterAlertsByWeekStartDate(List<WeeklyAdherence> weeklyAdherences, Date weekStartDate) {
+        List<WeeklyAdherence> filteredAlerts = new ArrayList();
+        CollectionUtils.select(weeklyAdherences, getSelectorForDate(weekStartDate), filteredAlerts);
+        return filteredAlerts;
+
+    }
+
+
+    private Predicate getSelectorForClinic(final String clinicName) {
+        return new Predicate() {
+            @Override
+            public boolean evaluate(Object o) {
+                WeeklyAdherence weeklyAdherence = (WeeklyAdherence) o;
+                return weeklyAdherence.getClinicName().equals(clinicName);
+            }
+        };
+    }
+
+    private Predicate getSelectorForDate(final Date weekStartDate) {
+        return new Predicate() {
+            @Override
+            public boolean evaluate(Object o) {
+                WeeklyAdherence weeklyAdherence = (WeeklyAdherence) o;
+                return weeklyAdherence.getWeekStartDate().equals(weekStartDate);
+            }
+        };
     }
 
 
