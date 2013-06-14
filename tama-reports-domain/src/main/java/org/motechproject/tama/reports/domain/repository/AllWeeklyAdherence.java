756c7189e1a224b7c7b2ba426e3778c6d29d1929
Vikram Rao <vikramra@thoughtworks.com>
2013-06-14 10:52:30 +0530

    Vikram | weekly missed dosage log

--- tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/repository/AllWeeklyAdherence.java
+++ tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/repository/AllWeeklyAdherence.java
@@ -5,9 +5,11 @@ import org.motechproject.tama.reports.domain.WeeklyAdherence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.JpaQueryCreator;
 
+import java.util.List;
+
 
 public interface AllWeeklyAdherence extends JpaRepository<WeeklyAdherence, Long> {
 
-    public WeeklyAdherence findWeeklyAdherenceById(String patient_document_id);
+    public List<WeeklyAdherence> findWeeklyAdherenceById(String patient_document_id);
 }
 
