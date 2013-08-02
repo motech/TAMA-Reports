3370c16e57fd7d3831331b64fff7c46161b466ed
Vikram Rao <vikramra@thoughtworks.com>
2013-08-02 12:13:56 +0530

    Vikram |  fix for defect 404 - weekly adherence mismatch | add boolean responded and remove dead code

--- tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/service/WeeklyAdherenceService.java
+++ tama-reports-domain/src/main/java/org/motechproject/tama/reports/domain/service/WeeklyAdherenceService.java
@@ -31,47 +31,9 @@ public class WeeklyAdherenceService {
 
     public void update(WeeklyAdherence weeklyAdherence) {
         WeeklyAdherence persistedWeeklyAdherence = new WeeklyAdherence();
-        List<WeeklyAdherence> weeklyAdherences = null;
         persistedWeeklyAdherence = allWeeklyAdherence.findByPatientDocumentIdAndClinicNameAndWeekStartDate(weeklyAdherence.getPatientDocumentId(),weeklyAdherence.getClinicName(),weeklyAdherence.getWeekStartDate());
 
         persistedWeeklyAdherence.merge(weeklyAdherence);
         allWeeklyAdherence.save(persistedWeeklyAdherence);
     }
-
-    private List<WeeklyAdherence> filterAlertsByClinicName(List<WeeklyAdherence> weeklyAdherences, String clinicName) {
-        List<WeeklyAdherence> filteredAlerts = new ArrayList();
-        CollectionUtils.select(weeklyAdherences, getSelectorForClinic(clinicName), filteredAlerts);
-        return filteredAlerts;
-
-    }
-
-    private List<WeeklyAdherence> filterAlertsByWeekStartDate(List<WeeklyAdherence> weeklyAdherences, Date weekStartDate) {
-        List<WeeklyAdherence> filteredAlerts = new ArrayList();
-        CollectionUtils.select(weeklyAdherences, getSelectorForDate(weekStartDate), filteredAlerts);
-        return filteredAlerts;
-
-    }
-
-
-    private Predicate getSelectorForClinic(final String clinicName) {
-        return new Predicate() {
-            @Override
-            public boolean evaluate(Object o) {
-                WeeklyAdherence weeklyAdherence = (WeeklyAdherence) o;
-                return weeklyAdherence.getClinicName().equals(clinicName);
-            }
-        };
-    }
-
-    private Predicate getSelectorForDate(final Date weekStartDate) {
-        return new Predicate() {
-            @Override
-            public boolean evaluate(Object o) {
-                WeeklyAdherence weeklyAdherence = (WeeklyAdherence) o;
-                return weeklyAdherence.getWeekStartDate().equals(weekStartDate);
-            }
-        };
-    }
-
-
 }
